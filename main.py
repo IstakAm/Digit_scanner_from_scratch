9f2fc3773fb39285d0ef4c6b3c5e21dfdcdcce22
BuildTools <unconfigured@null.spigotmc.org>
2023-01-06 19:16:46 +0330

    major changes

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 import os
 from keras.datasets import mnist
 
+np.set_printoptions(suppress=True)
+
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -64,17 +66,17 @@ class Network:
 
     def error(self, layer):
         if layer == self.n_layers - 1:
-            r = np.dot(SoftMax(self.outputs), np.subtract(self.outputs, self.predicted).T)
+            r = np.multiply(SoftMax(self.outputs), np.subtract(self.outputs, self.predicted))
         else:
-            r = np.dot(SoftMax(self.Layers[layer].outputs),
-                       np.sum(np.dot(self.error(layer + 1), self.Layers[layer].outputs))).T
+            r = np.multiply(SoftMax(self.Layers[layer].outputs),
+                            np.sum(np.multiply(self.error(layer + 1), self.Layers[layer].outputs)))
         return np.mean(r)
 
     def WeightDelta(self, layer):
         if layer == 0:
-            return np.dot(self.inputs, self.error(layer))
+            return np.multiply(self.inputs, self.error(layer))
         else:
-            return np.dot(self.Layers[layer - 1].outputs, self.error(layer))
+            return np.multiply(self.Layers[layer - 1].outputs, self.error(layer))
 
     def BiasDelta(self, layer):
         return self.error(layer)
@@ -98,31 +100,39 @@ class Network:
     def train(self, inputs, predicted, learning_rate):
         self.inputs = inputs
         self.predicted = predicted
-        for layer in range(self.n_layers):
-            tempo = self.Layers[layer].weights
-            temp2 = np.dot(self.WeightDelta(layer), learning_rate).T
+        for i in range(10000):
+            self.forward(inputs)
+            for layer in range(self.n_layers):
+                if layer == self.n_layers - 1:
+                    self.Layers[layer].weights = np.subtract(self.Layers[layer].weights,
+                                                             np.multiply(self.WeightDelta(layer), learning_rate).T)
 
-            self.Layers[layer].weights = np.subtract(self.Layers[layer].weights,
-                                                     np.dot(self.WeightDelta(layer), learning_rate).T)
-            self.Layers[layer].biases = np.subtract(self.Layers[layer].biases,
-                                                    np.dot(self.BiasDelta(layer), learning_rate).T)
+                else:
+                    self.Layers[layer].weights = np.subtract(self.Layers[layer].weights,
+                                                             np.multiply(self.WeightDelta(layer), learning_rate).T)
+                self.Layers[layer].biases = np.subtract(self.Layers[layer].biases,
+                                                        np.multiply(self.BiasDelta(layer), learning_rate))
 
-        self.Save()
+        # self.Save()
 
 
 nn = Network(784, 3, 12, 10, "first")
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
+last = 0
 for index in range(len(train_X)):
-    nn.forward(np.reshape(train_X[index], (1, 784)))
-    nn.train(np.reshape(train_X[index], (1, 784)), toPredicted(train_y[index]), 0.3)
-print("trained")
+    nn.train(np.reshape(train_X[index], (1, 784)), toPredicted(train_y[index]), -0.1)
+    print(index)
+
+
 right = 0
 wrong = 0
 for index in range(len(test_X)):
     nn.forward(np.ndarray.flatten(test_X[index]))
-
     if np.argmax(nn.outputs) == test_y[index]:
         right += 1
     else:
         wrong += 1
+print("rights percentage:")
 print(right * 100 / len(test_X))
+print("rights count:")
+print(right)
